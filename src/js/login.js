3cd94285e84d40a8a9e82ec2e4c0569181ca4028
Fjodor Tsumakov <notevil@laptop-gorgeous.public.wifi.ut.ee>
2025-10-01 14:51:06 +0300

    Basic page structure

---
+++ src/js/login.js
@@ -0,0 +1,13 @@
+// idk if we should use database or simple json file
+
+function login(login, password){
+    try {
+        // TODO
+    } catch(error){
+        console.error(error)
+        return false
+    }
+}
+
+// function login(login, token){
+// }
