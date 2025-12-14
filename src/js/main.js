a29ae050523929dacd9829813468d8eef2d9681f
NotEvil <notevil036@gmail.com>
2025-12-14 03:05:10 +0200

    yeah

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,9 @@
 import { createApp } from 'vue'
 import App from '../../App.vue'
 import router from './router.js' 
-import store from './store.js'
+import store from '@/store/index.js'
 
-// Create the Vue application instance
 const app = createApp(App)
-
-// Use the router and store
-app.use(router)
 app.use(store)
-
-// Mount the app to the #app div in index.html
+app.use(router)
 app.mount('#app')
