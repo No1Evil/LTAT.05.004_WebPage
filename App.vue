344f33ea6d746f6be4e98d5b006c4ab9ad129f86
NotEvil <notevil036@gmail.com>
2025-12-14 18:47:49 +0200

    remove checking auth in app.vue

--- App.vue
+++ App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app-container">
-    <Header :is-authenticated="isAuthenticated" @logout="Logout" /> 
+    <Header @logout="Logout" /> 
     
     <main id="mainBody"> 
       <router-view /> 
@@ -11,25 +11,9 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
-const isAuthenticated = ref(false);
-
-const checkAuthStatus = async () => {
-    try {
-        const response = await fetch("http://localhost:3000/auth/authenticate", {
-            credentials: 'include',
-        });
-        const data = await response.json();
-        isAuthenticated.value = data.authenticated;
-        console.log("Auth Status Checked:", isAuthenticated.value);
-    } catch (e) {
-        console.error("Authentication check failed:", e);
-        isAuthenticated.value = false;
-    }
-};
 
 const Logout = () => {
     fetch("http://localhost:3000/auth/logout", {
@@ -45,26 +29,6 @@ const Logout = () => {
         console.log("error login");
     });
 };
-
-const Login = () => {
-    fetch("http://localhost:3000/auth/login", {
-        credentials: 'include',
-    })
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        console.log('logged in, jwt created');
-    })
-    .catch((e) => {
-        console.log(e);
-        console.log("error login");
-    });
-};
-
-// When component is mounted will call async func
-onMounted(async () => {
-    await checkAuthStatus();
-});
 
 </script>
 
