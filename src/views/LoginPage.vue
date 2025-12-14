77968dec3c40b8aef29166fc315301be08eae989
NotEvil <notevil036@gmail.com>
2025-12-14 18:50:34 +0200

    Login page

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="app-container">
+        <div id="mainBody">
+            <Login></Login>
+            <Signup />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Login from "@/components/Login.vue";
+import Signup from "../components/Signup.vue";
+
+</script>
